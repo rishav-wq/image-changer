9015652123dd0306c688a919d061027a7a9abdb1
rishav_raj <145801777+rishav-wq@users.noreply.github.com>
2025-11-23 18:08:47 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://image-changer-delta.vercel.app/';
+const API_URL = 'https://image-changer-delta.vercel.app';
 
 const api = axios.create({
   baseURL: API_URL,
